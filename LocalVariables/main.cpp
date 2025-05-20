cf71077edfa29e519935e47c6829921f729342e3
NhomNhom0 <phingoc.nhomnhom@gmail.com>
2025-05-20 15:36:45 +0700

    Chapter7: Local Variables Quiz

--- LocalVariables/main.cpp
+++ LocalVariables/main.cpp
@@ -1,3 +1,18 @@
+/*
+ * Write a program that asks the user to enter two integers,
+ * one named smaller, the other named larger.
+ * 
+ * If the user enters a smaller value for the second integer,
+ * use a block and a temporary variable to swap the smaller and larger values.
+ * 
+ * Then print the values of the smaller and larger variables.
+ * 
+ * Add comments to your code indicating where each variable dies.
+ * 
+ * Note: When you print the values, smaller should hold the smaller input
+ * and larger the larger input, no matter which order they were entered in.
+ */
+
 #include "./include/swap.hpp"
 #include "./include/max.hpp"
 #include <iostream>
@@ -8,7 +23,7 @@ int main()
     int smaller;
     int larger;
     int max;
-    
+
     std::cout << "Enter an integer: ";
     std::cin >> smaller;
     std::cout << "Enter larger integer: ";
@@ -19,14 +34,11 @@ int main()
     {
         std::cout << "Swapping the values \n";
         std::pair<int, int> swapped = Quizz1::swap(smaller, larger);
-        std::cout << "The smaller value is: " << std::get<0>(swapped) << "\n";
-        std::cout << "The larger value is: " << std::get<1>(swapped) << "\n";
+        smaller = std::get<0>(swapped);
+        larger = std::get<1>(swapped);
     }
-    else
-    {
-        std::cout << "The smaller value is: " << smaller << "\n";
-        std::cout << "The larger value is: " << larger << "\n";
-    }
+    std::cout << "The smaller value is: " << smaller << "\n";
+    std::cout << "The larger value is: " << larger << "\n";
 
     return 0;
 }
